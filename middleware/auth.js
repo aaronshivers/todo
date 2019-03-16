44f19da92781bb4861726daaa30196fc4df68b90
aaronshivers <aaron@moonmail.org>
2019-03-16 11:56:56 -0500

    added comments to auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 const { User } = require('../models/users')
 
+// auth middleware
 module.exports = async (req, res, next) => {
 
   try {
@@ -8,21 +9,30 @@ module.exports = async (req, res, next) => {
     // get token from cookies
     const token = req.cookies.token
 
+    // reject if token doesn't exist
     if (!token) return res.status(401).render('error', { msg: 'Access Denied! No Token Provided.' })
 
+    // get secret from env
     const secret = process.env.JWT_SECRET
     
+    // verify token against the secret
     const decoded = await jwt.verify(token, secret)
 
     // find user by id
     const user = await User.findById(decoded._id)
 
+    // reject if no user is found in the DB
     if (!user) throw new Error()
 
+    // set req.user to hold the user data
     req.user = user
 
+    // move to next middleware
     next()
+    
   } catch (error) {
+    
+    // send error message
     res.status(400).render('error', { msg: 'Access Denied! Invalid Token.' })
   }
 }
