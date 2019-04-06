4a7d2a627fc7cf124d7f5ef1cf82486a16e26dbc
aaronshivers <aaron@moonmail.org>
2019-04-06 16:24:11 -0500

    setup caching for script.js

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,11 @@
 self.addEventListener('install', event => {
   console.log(`[Service Worker] Installing Service Worker...`, event)
+  event.waitUntil(caches.open('static').then(cache => {
+    console.log(`[Service Worker] Pre-Caching App Shell`)
+    cache.add(`/js/script.js`)
+  }))
 })
+
 
 self.addEventListener('activate', event => {
   console.log(`[Service Worker] Activating Service Worker...`, event)
@@ -8,5 +13,7 @@ self.addEventListener('activate', event => {
 })
 
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request))
+  event.respondWith(caches.match(event.request).then(response => {
+    response ? response : fetch(event.request)
+  }))
 })
