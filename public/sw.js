e4ade2fdc93e74b9094c44f807ff929740b41ce2
aaronshivers <aaron@moonmail.org>
2019-04-06 17:24:31 -0500

    added more files to be cached

--- public/sw.js
+++ public/sw.js
@@ -2,6 +2,18 @@ self.addEventListener('install', event => {
   console.log(`[Service Worker] Installing Service Worker...`, event)
   event.waitUntil(caches.open('static').then(cache => {
     console.log(`[Service Worker] Pre-Caching App Shell`)
+    cache.addAll([
+      `/`,
+      `/js/script.js`,
+      `/js/home-script.js`,
+      `/js/promise.js`,
+      `/js/fetch.js`,
+      `/css/bootstrap.min.css`,
+      `https://code.jquery.com/jquery-3.3.1.slim.min.js`,
+      `https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js`,
+      `https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js`,
+      `https://fonts.googleapis.com/css?family=Lato:400,700,400italic`
+    ])
     cache.add(`/js/script.js`)
   }))
 })
@@ -14,6 +26,10 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(caches.match(event.request).then(response => {
-    response ? response : fetch(event.request)
+    if (response) {
+      return response
+    } else {
+      fetch(event.request)
+    }
   }))
 })
