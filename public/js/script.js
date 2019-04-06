3441144e1207b48053bf41f4ca8dd5120ed1c5c7
aaronshivers <aaron@moonmail.org>
2019-04-06 06:55:39 -0500

    added service worker file

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,13 @@
+// service worker checker
+if ('serviceWorker' in navigator) {
+  navigator
+    .serviceWorker
+    .register('/sw.js')
+    .then(() => console.log(`Service Worker Registered!`))
+    .catch(err => console.log(err.message))
+}
+
+// navigation link updates
 const navLinks = document.getElementById('navLinks')
 
 document.cookie ? null : navLinks.innerHTML += `<li class="nav-item"><a class="nav-link" href="/login">Login</a></li>`
