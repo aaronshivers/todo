519b62cf0adfd5e32065592ed4ab5b34be99d375
aaronshivers <aaron@moonmail.org>
2019-04-07 11:10:44 -0500

    added an offline warning

--- public/sw.js
+++ public/sw.js
@@ -1,58 +1,64 @@
 const CACHE_STATIC_NAME = `static-v1`
 const CACHE_DYNAMIC_NAME = `dynamic-v1`
 
-self.addEventListener('install', event => {
-  console.log(`[Service Worker] Installing Service Worker...`, event)
-  event.waitUntil(caches.open(CACHE_STATIC_NAME).then(cache => {
-    console.log(`[Service Worker] Pre-Caching App Shell`)
-    cache.addAll([
-      `/`,
-      `/login`,
-      `/signup`,
-      `/todos`,
-      `/todos/new`,
-      `/users`,
-      `/users/profile`,
-      `/admin`,
-      `/js/script.js`,
-      `/js/home-script.js`,
-      `/js/promise.js`,
-      `/js/fetch.js`,
-      `/css/bootstrap.min.css`,
-      `https://code.jquery.com/jquery-3.3.1.slim.min.js`,
-      `https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js`,
-      `https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js`,
-      `https://fonts.googleapis.com/css?family=Lato:400,700,400italic`
-    ])
-  }))
+self.addEventListener('install', async event => {
+  try {
+    console.log(`[Service Worker] Installing Service Worker...`, event)
+        const cache = await caches.open(CACHE_STATIC_NAME)
+        if (cache) {
+      console.log(`[Service Worker] Pre-Caching App Shell`)
+            cache.addAll([
+        `/`,
+        `/offline`,
+        `/login`,
+        `/signup`,
+        `/js/script.js`,
+        `/js/home-script.js`,
+        `/js/promise.js`,
+        `/js/fetch.js`,
+        `/css/bootstrap.min.css`,
+        `https://code.jquery.com/jquery-3.3.1.slim.min.js`,
+        `https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js`,
+        `https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js`,
+        `https://fonts.googleapis.com/css?family=Lato:400,700,400italic`
+      ])
+    }
+  } catch (error) {
+    console.log(error.message)
+  }
 })
 
 
-self.addEventListener('activate', event => {
-  console.log(`[Service Worker] Activating Service Worker...`, event)
-  event.waitUntil(caches.keys().then(keyList => {
-    return Promise.all(keyList.map(key => {
+self.addEventListener('activate', async event => {
+  try {
+    console.log(`[Service Worker] Activating Service Worker...`, event)
+  
+    const keyList = await caches.keys()
+    keyList.map(key => {
       if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
         console.log(`[Service Worker] Removing Old Cache.`)
         return caches.delete(key)
       }
-    }))
-  }))
-  return self.clients.claim()
+    })
+    return self.clients.claim()    
+  } catch (error) {
+    console.log(error.message)
+  }
 })
 
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => {
+self.addEventListener('fetch', async event => {
+  try {
+    const response = await event.respondWith(caches.match(event.request))
     if (response) {
       return response
     } else {
-      return fetch(event.request).then(res => {
-        return caches.open(CACHE_DYNAMIC_NAME).then(cache => {
-          cache.put(event.request.url, res.clone())
-          return res
-        })
-      })
+      const res = await fetch(event.request)
+      const cache = await caches.open(CACHE_DYNAMIC_NAME)
+      cache.put(event.request.url, res.clone())
+      return res
     }
-  }))
+  } catch (error) {
+    const cache = await caches.open(CACHE_STATIC_NAME)
+    return cache.match(`/offline.html`)
+  }
 })
-
--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,7 @@ const router = express.Router()
 // GET /
 router.get('/', (req, res) => res.render('home'))
 
+// GET /
+router.get('/offline', (req, res) => res.render('offline'))
+
 module.exports = router
