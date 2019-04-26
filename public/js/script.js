4e55657c2a32a46f8d6e1ca4299a345e34d0a9f2
aaronshivers <aaron@moonmail.org>
2019-04-26 07:14:26 -0500

    disabled caching

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,11 @@
-// service worker checker
-if ('serviceWorker' in navigator) {
-  navigator
-    .serviceWorker
-    .register('/sw.js')
-    .then(() => console.log(`Service Worker Registered!`))
-    .catch(err => console.log(err.message))
-}
+// // service worker checker
+// if ('serviceWorker' in navigator) {
+//   navigator
+//     .serviceWorker
+//     .register('/sw.js')
+//     .then(() => console.log(`Service Worker Registered!`))
+//     .catch(err => console.log(err.message))
+// }
 
 // navigation link updates
 const navLinks = document.getElementById('navLinks')
--- public/sw.js
+++ public/sw.js
@@ -1,49 +1,49 @@
-const CACHE_STATIC_NAME = `static-v4`
-const CACHE_DYNAMIC_NAME = `dynamic-v1`
+// const CACHE_STATIC_NAME = `static-v4`
+// const CACHE_DYNAMIC_NAME = `dynamic-v1`
 
-self.addEventListener('install', async event => {
-  try {
-    console.log(`[Service Worker] Installing Service Worker...`, event)
-    const cache = await caches.open(CACHE_STATIC_NAME)
-    if (cache) {
-      console.log(`[Service Worker] Pre-Caching App Shell`)
-      cache.addAll([
-        `/`,
-        `/login`,
-        `/signup`,
-        `/js/script.js`,
-        `/js/home-script.js`,
-        `/js/promise.js`,
-        `/js/fetch.js`,
-        `/css/bootstrap.min.css`,
-        `https://code.jquery.com/jquery-3.3.1.slim.min.js`,
-        `https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js`,
-        `https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js`,
-        `https://fonts.googleapis.com/css?family=Lato:400,700,400italic`
-      ])
-    }
-  } catch (error) {
-    console.log(error.message)
-  }
-})
+// self.addEventListener('install', async event => {
+//   try {
+//     console.log(`[Service Worker] Installing Service Worker...`, event)
+//     const cache = await caches.open(CACHE_STATIC_NAME)
+//     if (cache) {
+//       console.log(`[Service Worker] Pre-Caching App Shell`)
+//       cache.addAll([
+//         `/`,
+//         `/login`,
+//         `/signup`,
+//         `/js/script.js`,
+//         `/js/home-script.js`,
+//         `/js/promise.js`,
+//         `/js/fetch.js`,
+//         `/css/bootstrap.min.css`,
+//         `https://code.jquery.com/jquery-3.3.1.slim.min.js`,
+//         `https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js`,
+//         `https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js`,
+//         `https://fonts.googleapis.com/css?family=Lato:400,700,400italic`
+//       ])
+//     }
+//   } catch (error) {
+//     console.log(error.message)
+//   }
+// })
 
 
-self.addEventListener('activate', async event => {
-  try {
-    console.log(`[Service Worker] Activating Service Worker...`, event)
+// self.addEventListener('activate', async event => {
+//   try {
+//     console.log(`[Service Worker] Activating Service Worker...`, event)
   
-    const keyList = await caches.keys()
-    keyList.map(key => {
-      if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-        console.log(`[Service Worker] Removing Old Cache.`)
-        return caches.delete(key)
-      }
-    })
-    return self.clients.claim()    
-  } catch (error) {
-    console.log(error.message)
-  }
-})
+//     const keyList = await caches.keys()
+//     keyList.map(key => {
+//       if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+//         console.log(`[Service Worker] Removing Old Cache.`)
+//         return caches.delete(key)
+//       }
+//     })
+//     return self.clients.claim()    
+//   } catch (error) {
+//     console.log(error.message)
+//   }
+// })
 
 // self.addEventListener('fetch', event => {
 //   event.respondWith(
